44375d785f834c9b73c8077dce718df4c5d10f2a
Rathan_Sreeramoju <you@example.com>
2020-07-11 07:11:48 +0530

    Main Screen

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,55 +1,134 @@
 package com.example.weatherapp;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import com.example.weatherapp.singletone.Constants;
+import com.example.weatherapp.singletone.ProgressDialog;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private AppBarConfiguration mAppBarConfiguration;
+//implements NavigationView.OnNavigationItemSelectedListener
+    public Toolbar toolbar;
+    public DrawerLayout drawerLayout;
+    public NavController navController;
+    public NavigationView navigationView;
+    private Constants constants;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        constants = Constants.getInstance(MainActivity.this);
+        ProgressDialog.loaderForPagination(MainActivity.this);
+
+
+
+        setupNavigation();
     }
 
+
+    public void setupNavigation()
+    {
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment);
+
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout);
+
+        NavigationUI.setupWithNavController(navigationView,navController);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+    }
+
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+
     }
 
     @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+        return NavigationUI.navigateUp(Navigation.findNavController(this,R.id.nav_host_fragment),drawerLayout);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        menuItem.setCheckable(true);
+        drawerLayout.closeDrawers();
+
+        int id = menuItem.getItemId();
+
+        switch (id)
+        {
+            case R.id.m_first:
+//                Toast.makeText(getApplicationContext(),"Default is Clicked!",Toast.LENGTH_SHORT).show();
+                navController.navigate(R.id.nav_montreal);
+                break;
+            case R.id.m_second:
+//                Toast.makeText(getApplicationContext(),"Second is Clicked!",Toast.LENGTH_SHORT).show();
+                navController.navigate(R.id.nav_london);
+                break;
+            case R.id.m_third:
+//                Toast.makeText(getApplicationContext(),"Third is Clicked!",Toast.LENGTH_SHORT).show();
+                navController.navigate(R.id.sanFranFragment);
+                break;
+            case R.id.m_fourth:
+//                Toast.makeText(getApplicationContext(),"fourth is Clicked!",Toast.LENGTH_SHORT).show();
+                navController.navigate(R.id.dubaiFragment);
+                break;
+            case R.id.m_fifth:
+//                Toast.makeText(getApplicationContext(),"fifth is Clicked!",Toast.LENGTH_SHORT).show();
+                navController.navigate(R.id.indiaFragment);
+                break;
+            case R.id.m_sixth:
+//                Toast.makeText(getApplicationContext(),"sixth is Clicked!",Toast.LENGTH_SHORT).show();
+                navController.navigate(R.id.moskowFragment);
+                break;
+
+//                <item
+//            android:id="@+id/m_fourth"
+//            android:title="Dubai" />
+//
+//        <item
+//            android:id="@+id/m_fifth"
+//            android:title="India" />
+//
+//        <item
+//            android:id="@+id/m_sixth"
+//            android:title="Moskow" />
+
+        }
+
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/weatherapp/modelpojoclass/Parent.java
@@ -0,0 +1,59 @@
+package com.example.weatherapp.modelpojoclass;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class Parent {
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("location_type")
+    @Expose
+    private String locationType;
+    @SerializedName("woeid")
+    @Expose
+    private Integer woeid;
+    @SerializedName("latt_long")
+    @Expose
+    private String lattLong;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocationType() {
+        return locationType;
+    }
+
+    public void setLocationType(String locationType) {
+        this.locationType = locationType;
+    }
+
+    public Integer getWoeid() {
+        return woeid;
+    }
+
+    public void setWoeid(Integer woeid) {
+        this.woeid = woeid;
+    }
+
+    public String getLattLong() {
+        return lattLong;
+    }
+
+    public void setLattLong(String lattLong) {
+        this.lattLong = lattLong;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("title", title).append("locationType", locationType).append("woeid", woeid).append("lattLong", lattLong).toString();
+    }
+
+}
